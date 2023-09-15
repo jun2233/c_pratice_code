09212c39da764e7492551320aa358df4b3423ffb
小贺 <1658163147@qq.com>
2023-09-15 19:46:54 +0800

    完成103，104，105题

---
+++ 105.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 105.c
+	> Author: 
+	> Mail: 
+	> Created Time: Mon 17 Apr 2023 04:14:54 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+int main() {
+    double H;
+    scanf("%lf", &H);
+    if(H > 55.4)printf("YES\n");
+    else printf("NO\n");
+}
