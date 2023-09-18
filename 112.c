b031668f64a94a9c7d16b6b6624547c0bd1481f8
小贺 <1658163147@qq.com>
2023-09-18 17:27:15 +0800

    完成109,110,111,112题

---
+++ 112.c
@@ -0,0 +1,26 @@
+/*************************************************************************
+	> File Name: 112.c
+	> Author: 
+	> Mail: 
+	> Created Time: Tue 18 Apr 2023 10:11:34 AM CST
+ ************************************************************************/
+
+#include<stdio.h>
+int main() {
+    char x;
+    scanf("%c", &x);
+    switch(x){
+        case 'a':{
+            printf("It is good\n");
+            break;
+        }
+        case 'b':{
+            printf("OMG\n");
+            break;
+        }
+        case 'c':{
+            printf("Yes!\n");
+            break;
+        }
+    }
+}
